ffb1e2f57d691a9ce6fd03350aa1a562f643b33c
Olivier Louvignes <olivier@mgcrea.io>
2025-11-18 13:17:41 +0100

    build: configure dual build system for ESM and CJS
    Add separate build paths for main package (ESM) and Babel plugin (CJS):
    
    - Add build:babel-plugin script using esbuild
    - Configure Babel to ignore babel plugin directory
    - Add CommonJS transform override for babel plugin source
    - Install required dependencies (@babel/types, esbuild, etc.)
    - Add package.json exports for dual module support
    - Export ./babel path pointing to bundled CJS file
    
    This solves the ESM/CommonJS compatibility issue by bundling the
    Babel plugin separately as a self-contained CommonJS module.

--- babel.config.js
+++ babel.config.js
@@ -10,4 +10,26 @@ export default {
       },
     ],
   ],
+  overrides: [
+    {
+      // Special handling for the babel plugin - output as CommonJS
+      test: /src\/babel\/index\.ts$/,
+      presets: [
+        [
+          "@babel/preset-typescript",
+          {
+            allowDeclareFields: true,
+          },
+        ],
+      ],
+      plugins: [
+        [
+          "@babel/plugin-transform-modules-commonjs",
+          {
+            importInterop: "babel",
+          },
+        ],
+      ],
+    },
+  ],
 };
